df60ea5b976c3a59c4ba98207c3b205502720e3d
“sobhan”912 <sobhan912@gmail.com>
2025-02-11 12:18:43 +0100

    Fix PWA status bar color

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1143809306"
+    serviceWorkerVersion: "196122414"
   }
 });
