8d5deca9558972bd9f893f73425f90db988d0bcf
“sobhan”912 <sobhan912@gmail.com>
2025-02-16 13:07:57 +0100

    Update Flutter web version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "dae08250bc0fa88089185e163bb00d7f",
+const RESOURCES = {"flutter_bootstrap.js": "a1f96b64df4cd1eafaaa37945bc6cd1a",
 "version.json": "f60311af9b9971714dc52030028af9ce",
 "index.html": "b840860194b155150f02de95c1c76f16",
 "/": "b840860194b155150f02de95c1c76f16",
-"main.dart.js": "3a746026ab254eb797c7585298c231ef",
+"main.dart.js": "1d15c25812b6b8820e8c1f4f0416894d",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
